73c15fec12a267b738d0dbed9a63234d0775a10c
denice <1176293952@qq.com>
2022-08-14 18:45:37 +0800

    线程状态

---
+++ src/day03/多线程/ThreadState.java
@@ -0,0 +1,38 @@
+package day03.多线程;
+
+/**
+ * @author 朔风
+ * @createTime 2022-08-14 18:37
+ */
+public class ThreadState {
+    public static void main(String[] args) throws InterruptedException {
+        T3 t3 = new T3();
+        System.out.println(t3.getName() + "当前状态: " + t3.getState());
+        t3.start();
+
+
+        while (Thread.State.TERMINATED != t3.getState()) {
+            System.out.println(t3.getName() + "当前状态: " + t3.getState());
+            Thread.sleep(500);
+        }
+
+        System.out.println(t3.getName() + "当前状态: " + t3.getState());
+    }
+}
+
+
+class T3 extends Thread {
+    @Override
+    public void run() {
+        while (true) {
+            for (int i = 1; i <= 10; i++) {
+                System.out.println("hi " + i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+}
