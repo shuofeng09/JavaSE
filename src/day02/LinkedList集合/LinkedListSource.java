1b15ead521cd8af983be19e2fabfd9db1f54ceba
denice <1176293952@qq.com>
2022-08-30 20:51:33 +0800

    LinkedList源码分析

---
+++ src/day02/LinkedList集合/LinkedListSource.java
@@ -0,0 +1,23 @@
+package day02.LinkedList集合;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author 朔风
+ * @createTime 2022-08-29 08:58
+ */
+@SuppressWarnings("all")
+public class LinkedListSource {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList<>();
+        linkedList.add(1);
+        linkedList.add(2);
+        linkedList.add(3);
+        System.out.println("HashSet = " + linkedList);
+
+    }
+
+}
