41da25fc642c5dd57528133de22bea42524cfc08
denice <1176293952@qq.com>
2022-08-30 20:51:45 +0800

    HashSet源码分析

---
+++ src/day02/HashSet集合/HashSetSource.java
@@ -0,0 +1,23 @@
+package day02.HashSet集合;
+
+import java.util.HashMap;
+
+/**
+ * @author 朔风
+ * @createTime 2022-08-30 20:47
+ */
+@SuppressWarnings("all")
+public class HashSetSource {
+    public static void main(String[] args) {
+
+        HashMap map = new HashMap();
+
+        map.put("1","xz");
+        map.put("2","xj");
+        map.put("3","xm");
+
+        System.out.println(map);
+
+
+    }
+}
