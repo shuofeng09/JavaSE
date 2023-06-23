63a52750c7e8999556ee9253ac50ab07e024840f
Wishlove09 <1176293852@qq.com>
2023-06-23 22:30:19 +0800

    main方法args参数测试

--- src/test/Test01.java
+++ src/test/Test01.java
@@ -5,30 +5,16 @@ package test;
  * @createTime 2022-07-06 13:01
  */
 public class Test01 {
-   /* public static void main(String[] args) {
-        String string = """
-                {
-                    "avatar": "/profile/2020/10/26/27e7fd58-0972-4dbf-941c-590624e6a886.png",
-                    "userName": "David",
-                    "nickName": "大卫",
-                    "password": "123456",
-                    "phonenumber": "15840669812",
-                    "sex": "0",
-                    "email": "David@163.com",
-                    "idCard": "210113199808242137"
-                }
-                """;
-        System.out.println(string);
 
-        String s = """
-                123123
-                123123
-                
-                456546
-                
-                123
-                """;
+    /**
+     * 测试命令行参数
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.print(args[i] + " ");
+        }
     }
-*/
 
 }
