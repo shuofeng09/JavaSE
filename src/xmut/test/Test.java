6aa9536667f8ec5772da042705baacc671c2ff24
Wishlove09 <1176293852@qq.com>
2023-12-08 11:26:17 +0800

    test

---
+++ src/xmut/test/Test.java
@@ -0,0 +1,19 @@
+package xmut.test;
+
+/**
+ * @author 朔风
+ * @date 2023-12-01 14:19
+ */
+public class Test {
+    public static void main(String[] args) {
+        try {
+            return;
+        } finally {
+            System.out.println();
+        }
+    }
+
+
+
+
+}
