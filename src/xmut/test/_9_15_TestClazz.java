dd10285292d098fa1790dab237830cf13e56a127
Wishlove09 <1176293852@qq.com>
2023-12-08 11:26:11 +0800

    test

--- src/xmut/test/_9_15_TestClazz.java
+++ src/xmut/test/_9_15_TestClazz.java
@@ -4,9 +4,8 @@ package xmut.test;
  * @author 朔风
  * @date 2023-09-15 10:58
  */
-public class _9_15_TestClazz {
-    public static void main(String[] args) {
-        char c = 'a' + 'b';
-        System.out.println(c);
+public abstract class _9_15_TestClazz {
+    public _9_15_TestClazz() {
+
     }
 }
